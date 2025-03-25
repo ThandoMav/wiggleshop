20c8fcca515435ccf3af8e65fde7ea47f3eb75b9
ThandoMav <eddymavweb@gmail.com>
2025-03-25 11:27:39 +0200

    first commit

--- components/BeautyAppntCta.js
+++ components/BeautyAppntCta.js
@@ -18,7 +18,7 @@ const About = () => {
             Book Your Beauty Journey Today – We Can’t Wait to Pamper You! 
             </h3>
             <button
-              className="mx-auto btnBeauty btn-whiteBeauty"
+              className="mx-auto btnBeauty btn-whiteBeauty text-primary-500"
               data-aos="fade-down"
               data-aos-delay="700"
             >
