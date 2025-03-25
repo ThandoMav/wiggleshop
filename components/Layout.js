9011dbaa0e33f85cd75c31813040d5fcc2a28408
ThandoMav <eddymavweb@gmail.com>
2025-03-25 12:20:10 +0200

    first commit

--- components/Layout.js
+++ components/Layout.js
@@ -5,9 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Head from 'next/head';
 import Header from './Header';
 import Footer from './BeautyFooter';
-import Copyright from './Copyright';
-
-import Script from 'next/script';
+import Copyright from './BeautyCopyright';
 
 export default function Layout({ children, title }) {
   return (
@@ -15,22 +13,10 @@ export default function Layout({ children, title }) {
       <Head>
         <title>{title ? title : 'Salon'}</title>
         <meta name="student blog" content="student Website" />
-        <meta name="monetag" content="7dfb0ee4ccebd9518e6723ad5b453b9b" />
-        <meta
-          name="09687ede797df4e3ed7eca4e8f37df3852d14ac8"
-          content="09687ede797df4e3ed7eca4e8f37df3852d14ac8"
-        />
+        
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Script
-        id="goterthing"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-                (function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('goomaphy.com',6594796,document.createElement('script'))
-          `,
-        }}
-      />
+      
       <ToastContainer position="bottom-center" limit={1} />
 
       <div className="'max-w-[1440px] mx-auto bg-page overflow-hidden relative'">
