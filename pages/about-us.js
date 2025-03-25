e32f1240b8e5e5962326702d47c3f1b62d1a8994
ThandoMav <eddymavweb@gmail.com>
2025-03-25 11:42:52 +0200

    first commit

--- pages/about-us.js
+++ pages/about-us.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from '../components/Layout';
 //import iimage from '../assets/img/about.png';
 //import Image from 'next/image';
-import Link from 'next/link';
+//import Link from 'next/link';
 //import hlengiwe from '../assets/hlengiwe.jpg';
 import Servicesa from '../components/BeautyServicess';
 import Dr1Banner from '../components/Dr1Banner';
@@ -10,7 +10,7 @@ import Dr1Banner from '../components/Dr1Banner';
 //import Faq from '../components/Faq';
 
 //import Testimonials from '../components/Testimonials';
-import { BsStarFill } from 'react-icons/bs';
+//import { BsStarFill } from 'react-icons/bs';
 
 export default function AboutMe() {
   return (
