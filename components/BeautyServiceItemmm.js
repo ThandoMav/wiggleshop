a740f665da40a6ac422b44a4e5baf697535460ea
ThandoMav <eddymavweb@gmail.com>
2025-03-25 14:26:49 +0200

    dgg

--- components/BeautyServiceItemmm.js
+++ components/BeautyServiceItemmm.js
@@ -5,13 +5,13 @@ import Image from 'next/image';
 
 export default function DrItem({ product }) {
   return (
-    <div className="group relative overflow-hidden hover:shadow-lg hover:dark:shadow-gray-800 rounded-md duration-500 p-6 text-center">
+    <div className="card group relative overflow-hidden hover:shadow-lg hover:dark:shadow-gray-800 rounded-md duration-500 p-6 text-center">
       <Image
         src={product.image}
         alt="hjh"
         width={500}
         height={500}
-        className="h-25 w-25 block mx-auto mb-2"
+        className="block mx-auto mb-2 object-cover h-64 w-full"
       ></Image>
       <Link href={`/post/${product.slug}`} className="hover:text-primary-500">{product.name}</Link>
       <div className="flex items-center justify-center mt-1">
