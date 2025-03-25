74e223b04821fc17dc56f12af533f4dfd14cbc62
ThandoMav <eddymavweb@gmail.com>
2025-03-25 13:22:49 +0200

    first commit

--- components/BeautyServicesss.js
+++ components/BeautyServicesss.js
@@ -25,7 +25,7 @@ const Banner = ({products}) => {
 
         <div className="grid lg:grid-cols-3 md:grid-cols-3 grid-cols-2 mt-8 gap-[30px] flex justify-center text-center px-6 ld:px-16">
           {products.map((product) => (
-            <CategoryItem key={product._id} category={product} />
+            <CategoryItem key={product._id} product={product} addToCartHandler={addToCartHandler} />
           ))}
         </div>
       </div>
